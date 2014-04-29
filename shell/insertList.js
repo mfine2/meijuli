3f7eaa3d8229fb553c184ca690e77bad9d654506
root <root@localhost.(none)>
2014-04-29 00:11:31 -0700

    add season 03

--- shell/insertList.js
+++ shell/insertList.js
@@ -4,7 +4,8 @@ var db;
 var collList;
 
 //var season = "01";
-var season = "02";
+//var season = "02";
+var season = "03";
 var series = "Friends";
 var done = null;
 
@@ -28,8 +29,11 @@ MongoClient.connect("mongodb://localhost:27017/meijuli", {native_parser: true}, 
 		//obj.mp4 = "mp4/Friends.Season01/Friends.S01E" + fixedI + ".1080p.BluRay.x264-maximersk.mp4";
 		//obj.srt = "srt/Friends.Season01/Friends.S01E" + fixedI + ".720p.BluRay.x264-PSYCHD.eng&chs.json";
 		
-		obj.mp4 = "mp4/Friends.Season02/friends.s02e" + fixedI + ".720p.bluray.x264-psychd.mp4";
-		obj.srt = "srt/Friends.Season02/Friends.S02E" + fixedI + ".720p.BluRay.x264-PSYCHD.eng&chs.json";
+		//obj.mp4 = "mp4/Friends.Season02/friends.s02e" + fixedI + ".720p.bluray.x264-psychd.mp4";
+		//obj.srt = "srt/Friends.Season02/Friends.S02E" + fixedI + ".720p.BluRay.x264-PSYCHD.eng&chs.json";
+
+		obj.mp4 = "mp4/Friends.Season03/friends.s03e" + fixedI + ".720p.bluray.x264-psychd.mp4";
+		obj.srt = "srt/Friends.Season03/friends.s03e" + fixedI + ".720p.bluRay.x264-psychd.eng&chs.json";
 		(function(i) {
 			collList.insert(obj, function(err, data){
 				if(err) {
